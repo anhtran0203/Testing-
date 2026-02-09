005b4bd71aff97e31eb6def0af13cf00fdd0d178
Anh Quynh Tran (3612) <anhquynhtran@C02H91GRQ05P.local>
2026-02-09 08:24:03 +0700

    fix run tag

--- runFeature.sh
+++ runFeature.sh
@@ -4,7 +4,12 @@ mkdir -p ./test/report
 mkdir -p ./test/output
 export startTime=$(date)
 export NODE_TLS_REJECT_UNAUTHORIZED=0
-node node_modules/@cucumber/cucumber/bin/cucumber-js src/features/*.feature --require src/step_definitions/ -f json:test/report/cucumber_report.json --tags "$1" 
+TAGS = "$1" | undefined
+if [ -z "$TAGS" ]; then
+    node node_modules/@cucumber/cucumber/bin/cucumber-js src/features/*.feature --require src/step_definitions/ -f json:test/report/cucumber_report.json
+else
+    node node_modules/@cucumber/cucumber/bin/cucumber-js src/features/*.feature --require src/step_definitions/ -f json:test/report/cucumber_report.json --tags "$1" 
+fi
 result=$?
 export endTime=$(date)
 node multiReport.js
